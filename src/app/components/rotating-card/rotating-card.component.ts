8a021aa17f2cd2168142a1b8f968aa061cf5df9e
aymensellaouti <aymen.sellaouti@gmail.com>
2024-09-30 14:10:42 +0100

    correction exercice roating card

--- src/app/components/rotating-card/rotating-card.component.ts
+++ src/app/components/rotating-card/rotating-card.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-rotating-card',
   templateUrl: './rotating-card.component.html',
-  styleUrls: ['./rotating-card.component.css']
+  styleUrls: ['./rotating-card.component.css'],
 })
 export class RotatingCardComponent {
-
+  // On va gérer :
+  name = 'sellaouti';
+  firstname = 'aymen';
+  job = 'teacher';
+  path = 'rotating_card_profile2.png';
 }
