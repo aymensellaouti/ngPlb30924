e1525af34a81cdbee9e407756630354603b09ba0
aymensellaouti <aymen.sellaouti@gmail.com>
2024-09-30 11:12:12 +0100

    exemple de binding

--- src/app/components/first/first.component.ts
+++ src/app/components/first/first.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./first.component.css']
 })
 export class FirstComponent {
+  name = 'FirstComponent';
 
+  message ='';
+  // Attributs => State
+  // Une variable qui gére la propriété hidden  de la balise p
+  isHidden: boolean = true;
+  // Méthodes => Beahivour (comportement)
+
+  showHide() {
+    this.isHidden = !this.isHidden;
+  }
+
+  changeMessage(newMessage: string) {
+    this.message = newMessage;
+  }
 }
