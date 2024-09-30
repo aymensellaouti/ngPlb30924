509653e65740efb0f81bab8b6556e4a7c7e4de1a
aymensellaouti <aymen.sellaouti@gmail.com>
2024-09-30 16:22:59 +0100

    correction exercice mini word

--- src/app/directives/mini-word/mini-word.component.ts
+++ src/app/directives/mini-word/mini-word.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-mini-word',
   templateUrl: './mini-word.component.html',
-  styleUrls: ['./mini-word.component.css']
+  styleUrls: ['./mini-word.component.css'],
 })
 export class MiniWordComponent {
-
+  // L'état du composant
+  color = '#ff0000';
+  fontFamily = 'garamond';
+  fontSize = 50;
 }
