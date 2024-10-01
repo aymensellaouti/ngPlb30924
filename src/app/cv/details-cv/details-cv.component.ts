4a18f5bf03afb7edc6250cfe324903968ff3d5b7
aymensellaouti <aymen.sellaouti@gmail.com>
2024-10-01 15:18:55 +0100

    todo de l'exercice

--- src/app/cv/details-cv/details-cv.component.ts
+++ src/app/cv/details-cv/details-cv.component.ts
@@ -10,4 +10,17 @@ export class DetailsCvComponent {
   cv: Cv | null = null;
 
   constructor() {}
+
+  // 1- Définir la route
+  // 2- Définir les méthodes dans le service CvService :
+    // 2-1 getCvById
+    // 2-2 deleteCv
+  // 3- Dans la carte le bouton details qui redirige vers la
+  // page détailsCvComponent
+  // 4- Dans détails :
+    // 4-1 récupérer le id et le chercher dans la liste
+      // 4-1-1 Ok afficher le cv
+      // 4-1-2 ko rediriger vers la liste des cvs
+    // 4-2 si click sur siupprimer on supprime puis on
+    // redirige vers la liste
 }
