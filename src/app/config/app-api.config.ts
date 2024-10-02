fcbcc6929d60678683677e8eddef5bb8fff7f789
aymensellaouti <aymen.sellaouti@gmail.com>
2024-10-02 10:46:30 +0100

    add swagger et api adresse

--- src/app/config/app-api.config.ts
+++ src/app/config/app-api.config.ts
@@ -1,3 +1,5 @@
 export const APP_API = {
   todo: 'https://jsonplaceholder.typicode.com/todos',
+  formationApiSwagger: 'https://apilb.tridevs.net/explorer/',
+  cv: 'https://apilb.tridevs.net/api/personnes/',
 };
