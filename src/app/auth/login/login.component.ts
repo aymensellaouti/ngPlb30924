17254e26eeb8d6849f3b9e66d75600def245fc4d
aymensellaouti <aymen.sellaouti@gmail.com>
2024-10-02 14:21:54 +0100

    Mise en place du login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Credentials } from '../dto/credentials.dto';
+import { APP_CONST } from 'src/app/config/app-constantes.config';
+import { APP_ROUTES } from 'src/app/config/app-routes.config';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,7 +12,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  constructor() {
-    localStorage.
+  authService = inject(AuthService);
+  toastr = inject(ToastrService);
+  router = inject(Router);
+  constructor() {}
+
+  login(credentials: Credentials) {
+    this.authService.login(credentials).subscribe({
+      next: () => {
+        this.toastr.success(`Bienvenu dans votre espace`);
+        this.router.navigate([APP_ROUTES.cv]);
+      },
+      error: (error) => {
+        this.toastr.error(`Veuillez vérifier vos credentials`);
+      }
+    })
   }
 }
--- src/app/config/app-constantes.config.ts
+++ src/app/config/app-constantes.config.ts
@@ -1,3 +1,4 @@
 export const APP_CONST = {
   defaultImage: 'cv.png',
+  token: 'token'
 };
